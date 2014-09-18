724e71cd02025c64e68a80e4bb963744c4af1fca
jmrealty <Gobears@aca800c5.ipt.aol.com>
2014-09-18 01:25:06 -0700

    complete

---
+++ TipCalculator/TipCalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  TipCalculator
+//
+//  Created by John  on 9/18/14.
+//  Copyright (c) 2014 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
