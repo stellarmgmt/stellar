3482efa7f50e2fee79755e3dcb035cf2c0c5e7d3
jmrealty <Gobears@aca800c5.ipt.aol.com>
2014-09-17 23:53:21 -0700

    Initial Commit

---
+++ TipCalculator/TipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by John  on 9/17/14.
+//  Copyright (c) 2014 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TipCalculator/TipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by John  on 9/17/14.
+//  Copyright (c) 2014 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
